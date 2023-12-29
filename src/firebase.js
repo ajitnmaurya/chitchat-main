af652e6fc5f67e672bd34cd05e75c60fc5430c63
ajitnmaurya <ak9430025@gmail.com>
2023-12-30 02:13:03 +0530

    chitchat

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxdfAe7nyU14m825sTlMmEHtrUkxZPcf8",
+  authDomain: "chitchat-ajit.firebaseapp.com",
+  projectId: "chitchat-ajit",
+  storageBucket: "chitchat-ajit.appspot.com",
+  messagingSenderId: "650233149851",
+  appId: "1:650233149851:web:43dce8223cbce231e6493e"
+};
+
+
+export const app = initializeApp(firebaseConfig);
